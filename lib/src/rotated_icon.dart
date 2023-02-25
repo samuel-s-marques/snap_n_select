17c0c3898b5bbe8417e138f4a708bec63866354b
Samuel Marques <samuel.s.marques@protonmail.com>
2023-02-25 13:31:33 -0300

    feat: create a rotation animated icon
    This Widget is used to create a rotation animated icon, by detecting the device's orientation.

---
+++ lib/src/rotated_icon.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:native_device_orientation/native_device_orientation.dart';
+
+class RotatedIcon extends StatefulWidget {
+  const RotatedIcon(this.icon, {super.key});
+
+  final Icon icon;
+
+  @override
+  State<RotatedIcon> createState() => _RotatedIconState();
+}
+
+class _RotatedIconState extends State<RotatedIcon> {
+  double turns = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NativeDeviceOrientationReader(
+      useSensor: true,
+      builder: (BuildContext context) {
+        final NativeDeviceOrientation orientation = NativeDeviceOrientationReader.orientation(context);
+
+        switch (orientation) {
+          case NativeDeviceOrientation.portraitUp:
+            turns = 0;
+            break;
+          case NativeDeviceOrientation.portraitDown:
+            turns = 1.0 / 2.0;
+            break;
+          case NativeDeviceOrientation.landscapeLeft:
+            turns = 1.0 / 4.0;
+            break;
+          case NativeDeviceOrientation.landscapeRight:
+            turns = -(1.0 / 4.0);
+            break;
+          case NativeDeviceOrientation.unknown:
+            turns = 0;
+            break;
+        }
+
+        return AnimatedRotation(
+          turns: turns,
+          duration: const Duration(milliseconds: 100),
+          child: widget.icon,
+        );
+      },
+    );
+  }
+}
